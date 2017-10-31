1292654846c6cfc2f339dc51d80fd206bc8a1731
Eogan Sullivan <eogan.sullivan@outlook.com>
2017-10-31 15:20:14 +0000

    Added MapReduce Method
    Map Reduce method

--- src/mongoOperation.java
+++ src/mongoOperation.java
@@ -9,6 +9,8 @@ import com.mongodb.Block;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.MapReduceCommand;
+import com.mongodb.MapReduceOutput;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -67,6 +69,38 @@ public class mongoOperation {
 	    else
 	    	JOptionPane.showMessageDialog(null,"Notice not resolved cannot delete");
 	    
+		
+	}
+	
+	public void MapReduce()
+	{
+		 String map = "function() { "+
+				 
+				              "var category; " + 				 
+				              "if ( this.pages >= 250 ) "+ 
+				              "category = 'Big Books'; " +
+				              "else " +
+				              "category = 'Small Books'; "+ 
+				              "emit(category, {name: this.name});}";
+				 
+				     
+				 
+				    String reduce = "function(key, values) { " +				 
+				                             "var sum = 0; " +
+				                             "values.forEach(function(doc) { " +
+				                             "sum += 1; "+
+				                             "}); " +
+				                             "return {books: sum};} ";
+				 
+				   
+				 
+				   MapReduceCommand cmd = new MapReduceCommand(null, reduce, reduce, reduce, null, null); //new MapReduceCommand(books, map, reduce,
+				 
+				    //  null, MapReduceCommand.OutputType.INLINE, null);
+				 				  				 
+				    MapReduceOutput out = new MapReduceOutput(null, null, null, null);// = books.mapReduce(cmd);				 
+				  
+
 		
 	}
 
